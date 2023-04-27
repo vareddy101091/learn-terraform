c5325384da4809c18b1122cb1d636e1d2ba1c09b
Anudeep Reddy Vanga <vareddy101091@gmail.com>
2023-04-27 17:12:23 +0530

    Terraform Instances

---
+++ 03-loops/count/main.tf
@@ -0,0 +1,16 @@
+resource "null_resource" "null" {
+  count = 10
+}
+
+resource "null_resource" "fruits" {
+  count = length(var.fruits)
+
+  provisioner "local-exec" {
+    command = "echo Fruit Name -  ${var.fruits[count.index]}"
+  }
+
+}
+
+variable "fruits" {
+  default = [ "apple", "banana", "orange"]
+}
