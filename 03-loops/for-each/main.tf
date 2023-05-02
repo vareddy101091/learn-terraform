3d57a38800b970578c0fd5050290937b1b6bdaa6
Anudeep Reddy Vanga <vareddy101091@gmail.com>
2023-05-02 16:22:32 +0530

    Terraform Instances

---
+++ 03-loops/for-each/main.tf
@@ -0,0 +1,62 @@
+resource "null_resource" "fruits" {
+
+  for_each = var.fruits
+
+  provisioner "local-exec" {
+    command = "echo Fruit Name - ${each.key} - ${each.value}"
+    //command = "echo  ${length(var.fruits)}"
+  }
+
+}
+
+resource "null_resource" "fruits1" {
+
+  for_each = var.fruits1
+
+  provisioner "local-exec" {
+    command = "echo Fruit Name - ${each.value["name"]} - ${each.value["count"]}"
+    //command = "echo  ${length(var.fruits)}"
+  }
+
+}
+
+variable "fruits1" {
+  default = {
+    apple = {
+      name = "apple"
+      count = 10
+    }
+    orange = {
+      name = "orange"
+      count = 200
+    }
+    banana = {
+      name = "banana"
+      count = 100
+    }
+  }
+}
+
+variable "fruits" {
+  default = {
+    apple   = 10
+    banana  = 100
+    oranges = 200
+  }
+}
+
+
+// For-each on a list
+variable "vegetables" {
+  default = ["carrot ", "capsicum"]
+}
+
+resource "null_resource" "vegetables" {
+
+  for_each = toset(var.vegetables)
+
+  provisioner "local-exec" {
+    command = "echo Fruit Name - ${each.key}"
+  }
+
+}
